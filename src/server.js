1bb52ebf94b2f59992b4416ad6fad5e576abd310
SamirBatista <samir_viana1@hotmail.com>
2023-02-08 20:21:56 -0300

    editCors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const todosRoutes = require("./tudos.routes");
 const app = express();
-
 const cors = require("cors");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(todosRoutes);
@@ -16,4 +11,6 @@ app.get("/teste", (req, res) => {
   return res.json("Up");
 });
 
-app.listen(3000, () => console.log("Server up in port 3000"));
+app.listen(3000, () => {
+  console.log("Server up in port 3000");
+});
